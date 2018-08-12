4af74c643e560438e436fdd7dbd78c6d8b42268f
natebecker11 <natebecker11@gmail.com>
2018-08-12 15:09:38 -0400

    started pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+//Global variables
+
+    // Array of possible words (wrdList)
+    // Array of letters of alphabet (azList)
+
+
+
+// Pick a word from an array of words
+
+
+// Create 'Source Word' (srcWord) object with the each letter of the word as a key/value pair
+
+
+
+
+
+
+
+
+// Create a line of underscores equal to word length
+    //  Create a 'Display Word' (disWord) object with the number of underscores as the current word as key/value pair, eg 0: '_', 1: '_' etc
+
+
+
+
+
+//Display number of guesses on the screen
+
+
+
+//Create an object with the guessed wrong letters (guessedWrong)
+
+//Create an object with the guessed right letters (guessedRight)
+
+
+
+
+
+
+
+
+//On user keyup, do some things
+    //  If the lowercase of the pressed key isn't within array a-z, return
+    //  
+    //  If the lowercase of the pressed key is within guessedWrong, return
+    //  
+    //  If the lowercase of the pressed key is within guessedRight, return
+    //  If the key press is a value within srcWord
+        // Iterate through every 
+    //
